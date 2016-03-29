58f5f0791902e30a7aed62c44b1f519b251bf9dd
Christopher Siems <csiems@gmail.com>
2016-03-28 21:17:39 -0700

    add menu search and logout

--- app/src/main/java/com/example/lsiems/myrestaurants/ui/RestaurantsListActivity.java
+++ app/src/main/java/com/example/lsiems/myrestaurants/ui/RestaurantsListActivity.java
@@ -3,16 +3,23 @@ package com.example.lsiems.myrestaurants.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.example.lsiems.myrestaurants.MyRestaurantsApplication;
 import com.example.lsiems.myrestaurants.R;
 import com.example.lsiems.myrestaurants.adapters.RestaurantListAdapter;
 import com.example.lsiems.myrestaurants.services.YelpService;
 import com.example.lsiems.myrestaurants.models.Restaurant;
+import com.firebase.client.Firebase;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -29,7 +36,9 @@ public class RestaurantsListActivity extends AppCompatActivity {
     private RestaurantListAdapter mAdapter;
     public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
     private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
     private String mRecentAddress;
+    private Firebase mFirebaseRef;
 
 
     @Override
@@ -38,13 +47,60 @@ public class RestaurantsListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
 
+        mFirebaseRef = MyRestaurantsApplication.getAppInstance().getFirebaseRef();
+
         mSharedPreferences = this.getSharedPreferences(getString(R.string.preference_file_key),
                 Context.MODE_PRIVATE);
         mRecentAddress = mSharedPreferences.getString("location", null);
+        mEditor = mSharedPreferences.edit();
 
         if (mRecentAddress != null) {
             getRestaurants(mRecentAddress);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        ButterKnife.bind(this);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getRestaurants(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                this.logout();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addToSharedPreferences(String location) {
+        mEditor.putString("location", location).commit();
+    }
+
+    private void logout() {
+        mFirebaseRef.unauth();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     private void getRestaurants(String location) {
